70a0559810a039f97332bee457ec985800e51a7c
Immortal <chris@fbi.al>
2015-07-27 20:55:45 -0400

    update version 0.0.2

--- FileDestroyer/FileDestroyer.Designer.cs
+++ FileDestroyer/FileDestroyer.Designer.cs
@@ -33,18 +33,34 @@
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.trashSelectedItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.encryptFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decryptFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.trashSelectedItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setPasskeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.useRandomKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
-            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
@@ -79,6 +95,49 @@
             this.columnHeader3.Text = "File path";
             this.columnHeader3.Width = 454;
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trashSelectedItemToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.encryptFileToolStripMenuItem,
+            this.decryptFileToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.settingsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 104);
+            // 
+            // trashSelectedItemToolStripMenuItem
+            // 
+            this.trashSelectedItemToolStripMenuItem.Name = "trashSelectedItemToolStripMenuItem";
+            this.trashSelectedItemToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.trashSelectedItemToolStripMenuItem.Text = "Trash selected item";
+            this.trashSelectedItemToolStripMenuItem.Click += new System.EventHandler(this.trashSelectedItemToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(173, 6);
+            // 
+            // encryptFileToolStripMenuItem
+            // 
+            this.encryptFileToolStripMenuItem.Name = "encryptFileToolStripMenuItem";
+            this.encryptFileToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.encryptFileToolStripMenuItem.Text = "Encrypt File";
+            this.encryptFileToolStripMenuItem.Click += new System.EventHandler(this.encryptFileToolStripMenuItem_Click);
+            // 
+            // decryptFileToolStripMenuItem
+            // 
+            this.decryptFileToolStripMenuItem.Name = "decryptFileToolStripMenuItem";
+            this.decryptFileToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.decryptFileToolStripMenuItem.Text = "Decrypt File";
+            this.decryptFileToolStripMenuItem.Click += new System.EventHandler(this.decryptFileToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(173, 6);
+            // 
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
@@ -95,6 +154,7 @@
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.textBox1);
@@ -104,6 +164,15 @@
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "File Selection";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(794, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "V&ersion 0.0.2 ";
             // 
             // button1
             // 
@@ -135,9 +204,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(188, 118);
+            this.button2.Location = new System.Drawing.Point(15, 118);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(249, 66);
+            this.button2.Size = new System.Drawing.Size(215, 51);
             this.button2.TabIndex = 3;
             this.button2.Text = "Trash Queue";
             this.button2.UseVisualStyleBackColor = true;
@@ -146,43 +215,121 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(443, 118);
+            this.button3.Location = new System.Drawing.Point(236, 118);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(249, 66);
+            this.button3.Size = new System.Drawing.Size(215, 51);
             this.button3.TabIndex = 4;
             this.button3.Text = "Clear Queue";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // contextMenuStrip1
+            // groupBox3
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.trashSelectedItemToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 26);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(619, 113);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(246, 85);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Cpu Information";
             // 
-            // trashSelectedItemToolStripMenuItem
+            // label4
             // 
-            this.trashSelectedItemToolStripMenuItem.Name = "trashSelectedItemToolStripMenuItem";
-            this.trashSelectedItemToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
-            this.trashSelectedItemToolStripMenuItem.Text = "Trash selected item";
-            this.trashSelectedItemToolStripMenuItem.Click += new System.EventHandler(this.trashSelectedItemToolStripMenuItem_Click);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(158, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "MEMORY USAGE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "0%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CPU USAGE:";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripSeparator2,
+            this.setPasskeyToolStripMenuItem,
+            this.useRandomKeyToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.settingsToolStripMenuItem.Text = "S&ettings";
+            // 
+            // setPasskeyToolStripMenuItem
+            // 
+            this.setPasskeyToolStripMenuItem.Name = "setPasskeyToolStripMenuItem";
+            this.setPasskeyToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.setPasskeyToolStripMenuItem.Text = "Set passkey";
+            this.setPasskeyToolStripMenuItem.Click += new System.EventHandler(this.setPasskeyToolStripMenuItem_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.Text = "change this pass";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(157, 6);
+            // 
+            // useRandomKeyToolStripMenuItem
+            // 
+            this.useRandomKeyToolStripMenuItem.Name = "useRandomKeyToolStripMenuItem";
+            this.useRandomKeyToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.useRandomKeyToolStripMenuItem.Text = "Use Random Key";
+            this.useRandomKeyToolStripMenuItem.Click += new System.EventHandler(this.useRandomKeyToolStripMenuItem_Click);
             // 
             // FileDestroyer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(881, 539);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "FileDestroyer";
             this.Text = "FileDestroyer";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -202,5 +349,20 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem trashSelectedItemToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem encryptFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decryptFileToolStripMenuItem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem setPasskeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem useRandomKeyToolStripMenuItem;
     }
 }
--- FileDestroyer/FileDestroyer.cs
+++ FileDestroyer/FileDestroyer.cs
@@ -2,17 +2,120 @@
 using System.IO;
 using System.Threading;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing;
+using ThreadState = System.Threading.ThreadState;
 
 namespace FileDestroyer
 {
     public partial class FileDestroyer : Form
     {
+        private PerformanceCounter pc;
+        private Thread dThread;
+        private byte[] key; 
+        delegate void SetCpuUsageCallback(string usage);
         public FileDestroyer()
         {
             InitializeComponent();
             textBox1.Text = Environment.CurrentDirectory.Replace("\\", "/") + "/";
+            pc = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+            dThread = new Thread(new ThreadStart(this.cpuUsage));
+            dThread.Start();
+            this.FormClosing += new FormClosingEventHandler(gen);
+            
+
         }
 
+        private void gen(object sender, FormClosingEventArgs e)
+        {
+           dThread.Abort();
+        }
+
+        /// <summary>
+        /// Credits to http://stackoverflow.com/questions/472906/converting-a-string-to-byte-array-without-using-an-encoding-byte-by-byte
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+        string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+        private void cpuUsage()
+        {
+            while (true)
+            {
+                Thread.Sleep(975);
+                string u = pc.NextValue().ToString("####");
+                this.SetCpuUsage(u);
+            }
+           
+        }
+
+        private void SetCpuUsage(string usage)
+        {
+            if (this.label3.InvokeRequired)
+            {
+                SetCpuUsageCallback scu = new SetCpuUsageCallback(SetCpuUsage);
+                this.Invoke(scu, new object[] {usage});
+            }
+            else
+            {
+                label3.Text = usage + "%";
+                string sLen = GC.GetTotalMemory(false).ToString();
+
+                if (GC.GetTotalMemory(true) >= (1 << 30))
+                {
+                    sLen = string.Format("{0} GB", GC.GetTotalMemory(true) >> 30);
+                }
+                else if (GC.GetTotalMemory(true) >= (1 << 20))
+                {
+                    sLen = string.Format("{0} MB", GC.GetTotalMemory(true) >> 20);
+                }
+                else if (GC.GetTotalMemory(true) >= (1 << 10))
+                {
+                    sLen = string.Format("{0} KB", GC.GetTotalMemory(true) >> 10);
+                }
+                else
+                {
+                    sLen = string.Format("{0} KB", GC.GetTotalMemory(true) >> 10);
+                }
+                label4.Text = sLen;
+            }
+            try
+            {
+                int val = int.Parse(usage);
+
+                if (val >= 80)
+                {
+                    label3.ForeColor = Color.Red;
+                }
+               else if (val >= 40)
+                {
+                    label3.ForeColor = Color.Coral;
+                }
+               else
+                {
+                    label3.ForeColor = Color.Green;
+                }
+
+            }
+            catch (Exception e)
+            {
+              
+            }
+          
+            
+        
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -57,40 +160,75 @@ namespace FileDestroyer
 
         private void trashSelectedItemToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("No files selected!");
+                return;
+            }
+            int i = 0;
             if (
                 MessageBox.Show(
                     "Are you sure you want to do this you will never get this file back again!", "Are you sure?",
                     MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
+                Thread[] threads = new Thread[listView1.SelectedItems.Count];
                 foreach (ListViewItem lvi in listView1.SelectedItems)
                 {
                     string path = lvi.SubItems[2].Text;
+                    textBox1.Text = path;
                     Thread t = new Thread(() => trashFile(path));
+                    threads[i] = t;
                     t.Start();
-                    //trashFile(path);
                     lvi.Remove();
+                    i++;
                 }
             }
         }
 
         private void trashQueue()
         {
+            int count = listView1.Items.Count;
+            if (count == 0)
+            {
+                MessageBox.Show("No files in the queue!");
+                return;
+            }
+
+            Thread[] threads = new Thread[count];
+
             if (
                 MessageBox.Show(
                     "Are you sure you want to do this you will never get this file back again!", "Are you sure?",
                     MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
+                int i = 0;
                 foreach (ListViewItem lvi in listView1.Items)
                 {
                     string path = lvi.SubItems[2].Text;
-
+                    textBox1.Text = path;
                     Thread t = new Thread(() => trashFile(path));
+                    threads[i] = t;
                     t.Start();
-                    //trashFile(path);
                     lvi.Remove();
+                    i++;
+
+                }
+                int removed = 0;
+                foreach (Thread myThread in threads)
+                {
+
+                    if (!myThread.IsAlive)
+                    {
+                        removed++;
+                    }
                 }
 
-                MessageBox.Show("Queue has been trashed!");
+                if (removed == count)
+                {
+                    MessageBox.Show("All files removed successfully!");
+                    GC.Collect();
+                }
+                
             }
         }
 
@@ -103,22 +241,25 @@ namespace FileDestroyer
                     MessageBox.Show("File does not exist!");
                     return;
                 }
+                FileInfo fi = new FileInfo(filePath);
+                /* Removed 7/27/2015
                 byte[] tempFile = File.ReadAllBytes(filePath);
-                byte[] newData = new byte[tempFile.Length];
-                Random r = new Random();
+                byte[] newData = new byte[tempFile.Length];*/
+                byte[] newData = new byte[fi.Length];
+              /* 
+              Random r = new Random();
                 byte[] secureKey = new byte[256];
                 r.NextBytes(secureKey);
-                tempFile = DexCryptMin.Encrypt(tempFile, secureKey);
+                tempFile = DexCryptMin.Encrypt(tempFile, secureKey);*/
 
                 for (int i = 0; i < newData.Length; i++)
                 {
-                    newData[i] = 0x00;
-                    tempFile[i] = 0x00;
+                    newData[i] = 0x0;
                 }
 
                 using (Stream s = File.Open(filePath, FileMode.Open))
                 {
-                    s.Position = 0x00;
+                    s.Position = 0x0;
                     s.Write(newData, 0, newData.Length);
                     s.Close();
                 }
@@ -142,6 +283,77 @@ namespace FileDestroyer
             {
                 lvi.Remove();
             }
+        }
+
+
+        private void encryptFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("No files selected!");
+                return;
+            }
+            int i = 0;
+            
+                Thread[] threads = new Thread[listView1.SelectedItems.Count];
+                foreach (ListViewItem lvi in listView1.SelectedItems)
+                {
+                    string path = lvi.SubItems[2].Text;
+                    textBox1.Text = path;
+                    Thread t = new Thread(() => encFile(path));
+                    threads[i] = t;
+                    t.Start();
+                    lvi.BackColor = Color.IndianRed;
+                    i++;
+                }
+            
+        }
+
+        private void decryptFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("No files selected!");
+                return;
+            }
+            int i = 0;
+
+            Thread[] threads = new Thread[listView1.SelectedItems.Count];
+            foreach (ListViewItem lvi in listView1.SelectedItems)
+            {
+                string path = lvi.SubItems[2].Text;
+                textBox1.Text = path;
+                Thread t = new Thread(() => dencFile(path));
+                threads[i] = t;
+                t.Start();
+                lvi.BackColor = Color.Aquamarine;
+                i++;
+            }
+
+        }
+
+        private void setPasskeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            key = GetBytes(toolStripTextBox1.Text);
+        }
+
+        private void encFile(string path)
+        {
+            byte[] encB = DexCryptMin.Encrypt(File.ReadAllBytes(path), key);
+            File.WriteAllBytes(path,encB);
+        }
+
+        private void dencFile(string path)
+        {
+            byte[] dncB = DexCryptMin.Decrypt(File.ReadAllBytes(path), key);
+            File.WriteAllBytes(path, dncB);
+        }
+
+        private void useRandomKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            key = new byte[256];
+            Random r = new Random();
+            r.NextBytes(key);
         }
     }
 }
